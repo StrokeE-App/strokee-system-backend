7924322a77386b97bcb44b507a7258bc998730c7
Carlos29ACC <carlos.caceres@e-hps.com>
2025-01-05 18:58:30 -0500

    Added Port

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import app from './boostrap'
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
     console.log('Server is running');
 })
