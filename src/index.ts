de3612d1d7695d341588552541c160e8ec059867
Carlos29ACC <carlos.caceres@e-hps.com>
2025-01-06 12:56:02 -0500

    Vercel setup

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import app from './boostrap'
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
     console.log('Server is running');
