5ac4b7747fe01de6c25b56ac2530e2c955387a22
Carlos29ACC <carlos.caceres@e-hps.com>
2025-01-23 23:57:37 -0500

    Added authentication

--- src/routes/operatorRoutes.ts
+++ src/routes/operatorRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { registerOperator, cancelEmergencyOperator, confirmEmergencyOperator } from "../controllers/operator/operatorController";
+import { verifyTokenWithRole } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/register", registerOperator);
-router.post("/assign-ambulance", confirmEmergencyOperator);
-router.post("/cancel-emergency", cancelEmergencyOperator);
+router.post("/assign-ambulance", verifyTokenWithRole(["operator"]), confirmEmergencyOperator);
+router.post("/cancel-emergency", verifyTokenWithRole(["operator"]), cancelEmergencyOperator);
 
 
 export default router;
