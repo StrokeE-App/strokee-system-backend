9cb5990a5d8cc92c672ce1d548dd761bea11f03d
Carlos29ACC <carlos.caceres@e-hps.com>
2025-03-18 18:20:58 -0500

    get all ambulances

--- src/controllers/operator/ambulanceController.ts
+++ src/controllers/operator/ambulanceController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { addAmbulance, deleteAmbulance, editAmbulance, getAmbulance } from "../../services/operators/ambulanceService";
+import { addAmbulance, deleteAmbulance, editAmbulance, getAmbulance, getAllAmbulances } from "../../services/operators/ambulanceService";
 
 export const addAmbulanceController = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -64,6 +64,24 @@ export const getAmbulanceController = async (req: Request, res: Response, next: 
             res.status(200).json({
                 message: result.message,
                 ambulance: result.ambulance,
+            });
+        } else {
+            res.status(400).json({
+                message: result.message,
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getAllAmbulancesController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await getAllAmbulances();
+        if (result.success) {
+            res.status(200).json({
+                message: result.message,
+                ambulances: result.ambulances,
             });
         } else {
             res.status(400).json({
