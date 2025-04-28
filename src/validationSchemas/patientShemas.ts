7ef35d5f51c37e2a3058810f1c57ecab5765da88
Carlos29acc <carlos.caceres@e-hps.com>
2025-04-28 09:32:20 -0500

    Added error messages for health center name validation

--- src/validationSchemas/patientShemas.ts
+++ src/validationSchemas/patientShemas.ts
@@ -60,34 +60,34 @@ export const patientSchema = Joi.object({
     }),
     emergencyContact: Joi.array().items(Joi.object({
         firstName: Joi.string().max(50).required().messages({
-            "string.max": "El nombre no puede tener más de 50 caracteres.",
-            "any.required": "El nombre es obligatorio.",
-            "string.base": "El nombre debe ser un string",
-            "string.empty": "El nombre no puede estar vacio."
+            "string.max": "El nombre del contacto de emergencia no puede tener más de 50 caracteres.",
+            "any.required": "El nombre del contacto de emergencia es obligatorio.",
+            "string.base": "El nombre del contacto de emergencia debe ser un string",
+            "string.empty": "El nombre del contacto de emergencia no puede estar vacio."
         }),
         lastName: Joi.string().max(50).required().messages({
-            "string.max": "El apellido no puede tener más de 50 caracteres.",
-            "any.required": "El apellido es obligatorio.",
-            "string.base": "El apellido debe ser un string",
-            "string.empty": "El apellido no puede estar vacio."
+            "string.max": "El apellido del contacto de emergencia no puede tener más de 50 caracteres.",
+            "any.required": "El apellido del contacto de emergencia es obligatorio.",
+            "string.base": "El apellido del contacto de emergencia debe ser un string",
+            "string.empty": "El apellido del contacto de emergencia no puede estar vacio."
         }),
         phoneNumber: Joi.string().length(10).pattern(/^\d+$/).required().messages({
-            "string.length": "El número de teléfono debe tener exactamente 10 dígitos.",
-            "string.pattern.base": "El número de teléfono solo debe contener números.",
-            "any.required": "El número de teléfono es obligatorio.",
-            "string.base": "El numero de telefono debe ser un string",
-            "string.empty": "El número de teléfono no puede estar vacio."
+            "string.length": "El número de teléfono del contacto de emergencia debe tener exactamente 10 dígitos.",
+            "string.pattern.base": "El número de teléfono del contacto de emergencia solo debe contener números.",
+            "any.required": "El número de del contacto de emergencia teléfono es obligatorio.",
+            "string.base": "El numero de telefono del contacto de emergencia debe ser un string",
+            "string.empty": "El número de teléfono del contacto de emergencia no puede estar vacio."
         }),
         email: Joi.string().email().required().messages({
-            "string.email": "El correo electrónico debe ser valido.",
-            "any.required": "El correo electrónico es obligatorio.",
-            "string.base": "El correo electrónico debe ser un string",
-            "string.empty": "El correo electrónico no puede estar vacio."
+            "string.email": "El correo electrónico del contacto de emergencia debe ser valido.",
+            "any.required": "El correo electrónico del contacto de emergencia es obligatorio.",
+            "string.base": "El correo electrónico del contacto de emergencia debe ser un string",
+            "string.empty": "El correo electrónico del contacto de emergencia no puede estar vacio."
         }),
         relationship: Joi.string().required().messages({
-            "any.required": "La relación es obligatoria.",
-            "string.base": "La relacion debe ser un string",
-            "string.empty": "La relacion no puede estar vacia."
+            "any.required": "La relación con contacto el emergencia es obligatoria.",
+            "string.base": "La relacion con contacto el emergencia debe ser un string",
+            "string.empty": "La relacion con contacto el emergencia no puede estar vacia."
         })
     })),
     medications: Joi.array().items(Joi.string()).required().messages({
